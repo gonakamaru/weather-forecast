b7735e67e2e18d71d083dcb7f5a7e9227d0948b2
Go Nakamaru <gonakamaru@gmail.com>
2025-12-16 17:40:11 +0900

    wip

--- src/orchestration/pipeline.py
+++ src/orchestration/pipeline.py
@@ -1,4 +1,27 @@
 class WeatherPipeline:
     """Orchestrates the weather forecast workflow."""
 
-    pass
+    def run(self):
+        chart = self._download_chart()
+
+        if not self._should_process(chart):
+            return
+
+        images = self._prepare_images(chart)
+        forecast = self._generate_forecast(images)
+        self._publish(images, forecast)
+
+    def _download_chart(self):
+        pass
+
+    def _should_process(self, chart):
+        pass
+
+    def _prepare_images(self, chart):
+        pass
+
+    def _generate_forecast(self, images):
+        pass
+
+    def _publish(self, images, forecast):
+        pass
